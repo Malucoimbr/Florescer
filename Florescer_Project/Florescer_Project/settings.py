e408d82fba1d45e45099f5f4cc73166dd6e16c67
Maria Luísa <marialuisacoimbra@gmail.com>
2023-10-16 21:42:53 -0300

    ajustes requirements

--- Florescer_Project/Florescer_Project/settings.py
+++ Florescer_Project/Florescer_Project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-c@rdlv4%5l=ss*cu2#zg_xprjn2m(buk4%(hr1+&d%%ro@bon)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "florescer.azurewebsites.net"]
 
 
 # Application definition
